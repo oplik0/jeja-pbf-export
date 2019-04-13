8303b77ad366887ed2934ed0f5ea976f40ab98eb
opliko95 <25460763+oplik0@users.noreply.github.com>
2019-04-13 23:05:25 +0200

    v0.03 - basic export to json fully working

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 from bs4 import BeautifulSoup
 from config import parse_config
 
@@ -11,80 +12,102 @@ class scrape():
         self.urls = parse_config("urls")
         # self.config = parse_config("config")
         self.session = requests.Session()
+        
+    
     def get_topic_urls(self):
         """
         Function used to get all not-ignored topic urls from group
         """
-        self.urls["topics"] = {}
+        self.urls["topics"] = []
         group_url = self.urls["group_url"]
         group_page = self.session.get(group_url)
-        if group_page.status_code != "200":
-            raise RuntimeError("Error "+group_page.status_code)
+        if group_page.status_code != 200:
+            raise RuntimeError("Error "+str(group_page.status_code))
 
         group_soup = BeautifulSoup(group_page.text, "html.parser")
         max_page_number = self.get_page_count(group_soup)
         tid = 0
         for i in range(max_page_number):
-            self.urls["topics"][tid] = {}
             page = self.session.get(group_url[:-5]+",{}.html".format(i))
             page_soup = BeautifulSoup(page.text, "html.parser")
             topic_urls = page_soup.find_all("a", {"class":"topic-title"})
             for url in topic_urls:
                 true_url="https://grupy.jeja.pl"+url["href"]
-                if true_url not in set(self.urls["ignore_urls"]+self.urls["kp_urls"]+self.urls["topic_urls"]):
+                if true_url not in set(self.urls["ignore_urls"]+self.urls["kp_urls"]):
+                    self.urls["topics"].append({})
                     self.urls["topics"][tid]["url"] = true_url
-                
-                if len(url.contents)==3:
-                    topic_title = url.contents[2]
-                else:
-                    topic_title = url.contents[0]
-                topic_title = topic_title.replace("\n", "").replace("\t", "")
-                self.urls["topics"][tid]["title"] = topic_title
-
+                    topic_title = url.contents[-1]
+                    topic_title = topic_title.replace("\n", "").replace("\t", "")
+                    self.urls["topics"][tid]["title"] = topic_title
+                    tid+=1
+        return self.urls
     def get_all_posts(self):
-        pass
-
+        for tid in enumerate(self.urls["topics"]):
+            self.get_posts_from_topic(tid[0])
 
     def get_posts_from_topic(self, tid):
-        self.urls["topics"][tid]["posts"] = {}
+        self.urls["topics"][tid]["posts"] = []
         topic_url = self.urls["topics"][tid]["url"]
         
         page = self.session.get(topic_url)
 
-        page_soup = BeautifulSoup(page, "html.parser")
+        page_soup = BeautifulSoup(page.text, "html.parser")
         max_page_number = self.get_page_count(page_soup)
         pid = 0
         for i in range(max_page_number):
             page = self.session.get(topic_url[:-5]+",0,{}.html".format(i))
-            page_soup = BeautifulSoup(page, "html.parser")
+            page_soup = BeautifulSoup(page.text, "html.parser")
             posts = page_soup.find_all("div", {"class":"komentarz kom-mar"})
             for post in posts:
-                self.urls["topics"][tid]["posts"][pid]["username"] = post.find("div", {"class":"nick"}).find("a").contents[0]
+                self.urls["topics"][tid]["posts"].append({})
+                nick_div = post.find("div", {"class":"nick"})
+                try:
+                    self.urls["topics"][tid]["posts"][pid]["username"] = nick_div.find("a").contents[0]
+                except AttributeError:
+                    self.urls["topics"][tid]["posts"][pid]["username"] = nick_div.contents[0]
                 self.urls["topics"][tid]["posts"][pid]["avatar"] = post.find("img", {"class":"komentarz-foto"})["src"].replace("90x90", "small")
                 post_contents = post.find("div", {"class":"text"})
                 post_content = ""
                 for content in post_contents:
                     post_content += str(content)
-                replacable_list = ['<a class="fancybox" data-fancybox="grupy" data-fancybox-group="fancybox" href="', '</a>', 
-                                   '">', '<img alt="" src="', '" style="max-width:510px"/>', '<a class="withUl" href="',
-                                   '" rel="nofollow', '" target="_blank', '<strong>', '</strong>', '<em>', '</em>', '<u>', '</u>',
-                                   '<iframe allowfullscreen="" frameborder="0" height="285" src="https://www.youtube.com/embed/',
-                                   '" width="510]</iframe>', '<br/>', "<s>", "</s>"]
-                to_replace_list = ['[url=', ']', '[/url]', '[img]', '[/img]', '[url=', '', '', '**', '**', '*', '*',
-                                   '__', '__', '[youtube]', '[/youtube]', '', "~~", "~~"]
-
-                for replacable, to_replace in replacable_list, to_replace_list:
-                    post_content.replace(replacable, to_replace)
+                post_content = self.jeja_to_markdown(post_content)
                 
                 self.urls["topics"][tid]["posts"][pid]["content"] = post_content
-
-
+                pid+=1
+        return self.urls
 
     def get_page_count(self, soup):
         pages = soup.find_all("a", {"class":"pagination-number"})
         if len(pages)>0:
             max_page_url = pages[-1]
-            max_page_number = int(max_page_url[max_page_url.rfind(",")+1:-5])
+            max_page_number = int(max_page_url["href"][int(max_page_url['href'].rfind(","))+1:-5])
         else:
-            max_page_number = 0
+            max_page_number = 1
         return(max_page_number)
+
+
+
+    def export(self):
+        self.get_topic_urls()
+        self.get_all_posts()
+        return self.urls
+    def export_to_file(self, filename):
+        with open(filename, "w") as f:
+            json.dump(self.export(), f, indent=2)
+
+    def jeja_to_markdown(self, text):
+        
+        text = text.replace("<strong>", "**").replace("</strong>", "**")
+        text = text.replace("<b>", "**").replace("</b>", "**")
+        text = text.replace("<em>", "*").replace("</em>", "*")
+        text = text.replace("<s>", "~~").replace("</s>", "~~")
+        text = text.replace("<br/>", "")
+        text = text.replace('blockquote class="quote">', ">").replace("</blockquote>", "")
+        text = re.sub(r'\<a .*href\=\"(\S+)\"[^>]*\>(.*)\<\/a\>', r'[\2](\1)', text)
+        text = re.sub(r'\<img.*src\=\"([^>"]*)\"[^>]*\>', r'![image](\1)', text)
+        text = re.sub(r'\<iframe.*src\=\"\/\/www\.youtube\.com\/embed\/([\S]+)\"[^>]*\>\<\/iframe\>', r'https://youtu.be/\1', text)
+        return str(text)
+
+if __name__=="__main__":
+    a = scrape()
+    a.export_to_file("result.json")
