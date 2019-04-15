7fec965b416453353dcaf12a9d6981709b20ba3f
opliko95 <25460763+oplik0@users.noreply.github.com>
2019-04-15 14:32:14 +0200

    v0.9

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from export import export
 from nodebb_import import nodebb_import
 ex = export.scrape()
-im = nodebb_import.nodebb_import()
+
 
 if __name__=="__main__":
     ex.export_to_file("export_result.json")
+    im = nodebb_import.nodebb_import()
     im.import_topics()
     im.import_posts()
     im.import_character_sheet_template()
