6ce7e6e15ab8e3f863d7c6a14c0b39e14e486a96
oplik0 <opliko.reg@protonmail.com>
2019-06-24 11:46:19 +0000

    No idea what were the differences, but this version works

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ ex = export.scrape()
 
 
 if __name__=="__main__":
+    print("starting export...")
     ex.export_to_file("export_result.json")
+    print("exported! Starting import...")
     im = nodebb_import.nodebb_import()
     im.import_topics()
+    print("imported topics")
     im.import_posts()
+    print("imported posts")
     im.import_character_sheet_template()
     print("Done!")
